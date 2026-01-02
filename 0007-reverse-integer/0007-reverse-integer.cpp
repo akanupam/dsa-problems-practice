28c2405a9586c8991e2e5173c5c379db781e4db3
Aditya Kumar Anupam <143707452+akanupam@users.noreply.github.com>
2026-01-02 11:32:01 +0530

    Time: 0 ms (100%), Space: 7.4 MB (100%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int reverse(int x) {
+        long long r=0;
+        while (x!=0){
+            r= r*10 + x%10;
+            x/=10;
+            if(abs(r)> INT_MAX){
+                return 0;
+            }
+        }
+        return r;
+    }
+};
